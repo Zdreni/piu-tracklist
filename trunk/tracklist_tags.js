7bd36a0fbbe4e2f5fe4206dc58a8756719940602
zdreni <zdreni@35f51612-9486-44a8-b249-40bf00a45cb4>
2016-09-01 22:44:29 +0000

    Updated UPF-2016 international round tracklist.
    git-svn-id: https://subversion.assembla.com/svn/piu-tracklist@40 35f51612-9486-44a8-b249-40bf00a45cb4

--- trunk/tracklist_tags.js
+++ trunk/tracklist_tags.js
@@ -43,20 +43,46 @@ function Tag( tag, fullChartDescr )
 }
 
 Tag( "UPF-2016-singles",
-     [ "1950  S-20",
-       "Karyawisata  S-20",
+     [ "Passacaglia  S-20",
+	   "Love is a Danger Zone (Cranky Mix)  S-20",
+	   "Lucid (PIU Edit)  S-20",
        "Move That Body!  S-20",
+	   "1950  S-20",
+       "Karyawisata  S-20",
+
+	   "Hypnosis (SynthWulf Mix)  S-21",
+	   "Dream to Nightmare  S-21",
+	   "Hardkore of the North  S-21",
+	   "Windmill  S-21",
+	   "Mental Rider  S-21",
        "Yog-Sothoth  S-21",
        "Robot Battle  S-21",
-       "Achluoias  S-22",
-       "1950  S-23",
-       "Yog-Sothoth  S-23" ] );
+
+       "Elise  S-22",
+	   "Love is a Danger Zone (Cranky Mix)  S-22",
+	   "Dream to Nightmare  S-22",
+	   "Achluoias  S-22",
+
+       "Ignis Fatuus (DM Ashura Mix)  S-23",
+	   "Flew Far Faster  S-23",
+	   "VVV  S-23",
+       //"Yog-Sothoth  S-23",
+	   "1950  S-23",
+     ] );
 
 Tag( "UPF-2016-doubles",
-     [ "Achluoias  D-24",
+     [ "Passacaglia  D-24",
+	   "Dream to Nightmare  D-24",
+	   "Love is a Danger Zone (Cranky Mix)  D-24",
+	   "Flew Far Faster  D-24",
+	   "Windmill  D-24",
        "Avalanche  D-24",
-       "1950  D-25",
-       "Yog-Sothoth  D-25" ] );
+	   "Achluoias  D-24",
+
+	   "Ignis Fatuus (DM Ashura Mix)  D-25",
+	   "Yog-Sothoth  D-25",
+       //"1950  D-25",
+     ] );
 
 Tag( "triples", [] );
 
