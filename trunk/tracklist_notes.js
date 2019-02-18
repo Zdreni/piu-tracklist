440cbd9d7b2024eb2a8299dcbe981b3b9fd917f2
zdreni <zdreni@35f51612-9486-44a8-b249-40bf00a45cb4>
2019-02-18 15:27:12 +0000

    Added Tracklist Chart Notes functionality
    git-svn-id: https://subversion.assembla.com/svn/piu-tracklist@179 35f51612-9486-44a8-b249-40bf00a45cb4

---
+++ trunk/tracklist_notes.js
@@ -0,0 +1,68 @@
+"use strict";
+
+function N( trackName, chartDescrs, note )
+{
+	if( ! Array.isArray( chartDescrs ) )
+		chartDescrs = [ chartDescrs ];
+
+	var track = FindTrack( tracklist, trackName );
+	for( var chartDescr of chartDescrs )
+	{
+		var chart = FindChart( track, chartDescr );
+		if( ! chart.shared.notes )
+			chart.shared.notes = [];
+		chart.shared.notes.push( note );
+	}
+}
+
+
+var BUILT_WITH_STOPS = "Whole chart is tricky stops";
+var HAS_SOME_STOPS = "Some parts of chart are tricky stops";
+
+
+function AddNotes()
+{
+	N( "Love Scenario", "S17", BUILT_WITH_STOPS )
+	N( "Uprock", "CZ", BUILT_WITH_STOPS )
+	N( "Everybody Got 2 Know", "S19", BUILT_WITH_STOPS )
+	N( "Come to Me @ 826", "S17", BUILT_WITH_STOPS )
+
+	N( "Tek -Club Copenhagen-", ["S16", "S17"], HAS_SOME_STOPS )
+	N( "Bad Apple!! feat. Nomico", "S15", HAS_SOME_STOPS )
+	N( "Miss's Story", "S18", "Small part with tricky stops" )
+
+	N( "Gun Rock", "CZ", "Extreme triples streams at the end" )
+	// naissance acz - jumps whole chart
+
+	// ugly dee - даблы холды
+
+	N( "Join the Party", "CZ", "central hold + steps" )
+	N( "Dance Vibrations", ["aCZ", "aNM"], "central hold + steps" )
+
+	// nobody какой-то там - стопы
+	// fire 2ne1 - mimimi таппинг
+
+	// final audition acz - футсвичи длинные
+
+	// Nakakapagpabagabag, 86 - 
+	N( "Nakakapagpabagabag", ["S18", "D19"], "holds with steps, stumble charts, needs 6.5x or av999" )
+	N( "86", ["S16", "S20", "D18", "D21"], "holds with steps, stumble charts, needs 6.5x or av999" )
+	// Shub Niggurath D17 - twists on center
+	// Hey U - jumps
+	// Feel My Happines D21 - brackets
+	// Karyawisata S20 - slowdown
+	// Moonlight S21 - slowdown
+	// De La Rue  D24 - blinking holds
+	// Vanish - Vanish
+	// Hardkore of the North  S21 - twists on the holds with slowdown
+	// VVV - whole chart is slow except of simply ending, needs av~900
+
+	N( "Destination  [SHORT]", "D18", "crab legs" ) //D20 - ебошащий стрим
+	N( "Pumptris (8Bit ver.)  [SHORT]", "D22", "crab legs" )
+
+	N( "Twist of Fate (feat. Ruriling)", ["S18", "D20"], "Main part has EXTREME slowdown" )
+	//N( "The Festival of Ghost2 (Sneak)", [], "Lot of gimmicks" )
+}
+
+
+initFuncs.push( AddNotes );
